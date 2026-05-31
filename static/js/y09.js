0e05d161a9f635d3f74bdb694e4459d4f8f8bb59
hfhhdgfsf <97849931+hfhhdgfsf@users.noreply.github.com>
2026-05-31 23:56:41 +0800

    feat: 重新生成按钮 - regenerateSeal全局函数

--- static/js/y09.js
+++ static/js/y09.js
@@ -1,6 +1,5 @@
  new Vue({
             el: '#app',
-            mounted() { window.vm = this; },
             data() {
                 return {tabIndex: 0,
                     sealName: '椭圆印章',
@@ -259,3 +258,11 @@
                 that.createSealEx2()
             }
         })
+        // 全局重绘函数 - 供原生按钮调用
+        window.regenerateSeal = function() {
+            var el = document.getElementById('app');
+            if (el && el.__vue__ && el.__vue__.createSealEx2) {
+                el.__vue__.createSealEx2();
+            }
+        };
+    
