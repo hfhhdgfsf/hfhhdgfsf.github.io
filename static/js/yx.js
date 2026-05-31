b5da0b37604cc087697bff065cfe106cd8ccfbe5
hfhhdgfsf <97849931+hfhhdgfsf@users.noreply.github.com>
2026-05-31 23:57:42 +0800

    feat: 重新生成按钮 - regenerateSeal全局函数

--- static/js/yx.js
+++ static/js/yx.js
@@ -1,4 +1,12 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?xml versi
+        // 全局重绘函数 - 供原生按钮调用
+        window.regenerateSeal = function() {
+            var el = document.getElementById('app');
+            if (el && el.__vue__ && el.__vue__.createSealEx2) {
+                el.__vue__.createSealEx2();
+            }
+        };
+    on="1.0" encoding="UTF-8"?>
 <Error>
   <Code>NoSuchKey</Code>
   <Message>The specified key does not exist.</Message>
