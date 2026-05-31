73082dcfafd00caaeb4c0f5e0873ec14dddea6e7
hfhhdgfsf <97849931+hfhhdgfsf@users.noreply.github.com>
2026-05-31 23:28:11 +0800

    fix: 暴露Vue实例到全局window.vm + 替换el-button为原生按钮

--- static/js/y12.js
+++ static/js/y12.js
@@ -1,5 +1,6 @@
-        new Vue({
+        var vm = new Vue({
             el: '#app',
+            mounted() { window.vm = this; },
             data() {
                 return {tabIndex: 0,
                     sealName: '方形印章',
@@ -7,7 +8,9 @@
                         {name: '宋体'},
                         {name: '仿宋'},
                         {name: '楷体'},
-                        {name: '黑体'},                    ],
+                        {name: '黑体'},
+                        {name: '汉仪长宋简'},
+                    ],
                     // 第一个字
                     input01: '方',
                     fontFamily01: '宋体',
