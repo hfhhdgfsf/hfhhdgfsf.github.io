f9c131900bd9c2d3879767a116fc18ad9033d352
hfhhdgfsf <97849931+hfhhdgfsf@users.noreply.github.com>
2026-05-31 23:28:07 +0800

    fix: 暴露Vue实例到全局window.vm + 替换el-button为原生按钮

--- static/js/y10.js
+++ static/js/y10.js
@@ -1,5 +1,6 @@
-        new Vue({
+        var vm = new Vue({
             el: '#app',
+            mounted() { window.vm = this; },
             data() {
                 return {tabIndex: 0,
                     
@@ -8,7 +9,9 @@
                         {name: '宋体'},
                         {name: '仿宋'},
                         {name: '楷体'},
-                        {name: '黑体'},                    ],
+                        {name: '黑体'},
+                        {name: '汉仪长宋简'},
+                    ],
                     // 公司
                     input01: '艾你图椭圆有限公司',
                     fontFamily01: '宋体',
