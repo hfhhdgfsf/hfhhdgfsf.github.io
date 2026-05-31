20a7ea4a7802427c4b7002cea020aad05e4fd872
hfhhdgfsf <97849931+hfhhdgfsf@users.noreply.github.com>
2026-06-01 00:17:29 +0800

    fix: 增删文字居中 - 圆弧中点固定不动，文字向中心聚拢/分散

--- static/js/y33.js
+++ static/js/y33.js
@@ -189,7 +189,7 @@
                     for (var i = 0; i < count; i++) {
                         c = chars[i];// 需要绘制的字符   
                         if (i == 0)
-                            context.rotate((5.95) * Math.PI / 6 - (count - 1) * (angle - 4 * Math.PI / (3 * count + 10)) / 2) // centered;
+                            context.rotate((5.95) * Math.PI / 6 - (count - 1) * angle / 2); // center fixed
                         else
                             context.rotate(angle);
                         context.save();
